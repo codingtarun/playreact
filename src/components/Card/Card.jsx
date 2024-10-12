9956519c06018fee5bb73e5e2cb7d6a79814d38a
coding.tarun <coding.tarun@gmail.com>
2024-10-12 21:02:44 +0530

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
 export const Card = ({ title, children }) => {
   return (
-    <div className="bg-white w-11/12 sm:w-3/4 md:w-3/5 rounded-md shadow-md hover:shadow-lg overflow-hidden border-solid border-1 border-zinc-200 hover:cursor-pointer">
+    <div className="bg-white w-11/12 sm:w-3/4 md:w-3/5 shadow-sm hover:shadow-lg overflow-hidden border-solid border-1 border-zinc-200">
       <div>
         <div className="mb-2 bg-zinc-200 ">
           <h1 className="font-semibold text-gray-950 text-sm p-2 uppercase">
